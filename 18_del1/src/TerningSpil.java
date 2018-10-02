2347b7c94f3c5c8dc705b69d5b09b051bee84a40
nikolajwassmann <nwassmann@hotmail.com>
2018-10-02 12:54:57 +0200

    Scanner og kommentar

--- 18_del1/src/TerningSpil.java
+++ 18_del1/src/TerningSpil.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class TerningSpil {
     public static void main (String[]args){
+
+        System.out.println("Dette er et terning spil");
+
+        // Scanner lavet, som giver brugeren mulighed for input //
+
+        Scanner Spil = new Scanner(System.in);
+
+
 
     }
 }
