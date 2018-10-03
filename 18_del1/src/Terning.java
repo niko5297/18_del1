38d2f5baf6fb1bd72acb6df463cde9b46e29c35d
nikolajwassmann <nwassmann@hotmail.com>
2018-10-03 08:05:11 +0200

    Lavet Terning klasse + noget andet

--- 18_del1/src/Terning.java
+++ 18_del1/src/Terning.java
@@ -1,3 +1,16 @@
 public class Terning {
-    public int Terning;
+
+    // Kaster terning
+    public int kast() {
+        float t1 = (float) Math.random();
+        float t2 = t1 * 5;
+        int t3 = Math.round(t2);
+        return t3 + 1;
+    }
+    public void kastGange(int g){
+        for(int k=0; k<g; k++){
+            System.out.print(kast() + " ");
+        }
+    }
+
 }
--- 18_del1/src/TerningSpil.java
+++ 18_del1/src/TerningSpil.java
@@ -1,13 +1,29 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class TerningSpil {
     public static void main (String[]args){
 
         System.out.println("Dette er et terning spil");
+        System.out.println("Spillet er mellem 2 personer");
+        System.out.println("Spiller nr.1 kaster");
 
         // Scanner lavet, som giver brugeren mulighed for input //
 
+        Terning tern = new Terning();
         Scanner Spil = new Scanner(System.in);
+
+        System.out.println("Tast 2 for at kaste terningen: ");
+
+        if (Spil.nextInt()==2){
+            tern.kast();
+            System.out.println(tern.kast());
+        }
+        else {
+            System.out.println("Forkert input... Tast 2 for at kaste terningen: ");
+        }
+
+
 
 
 
