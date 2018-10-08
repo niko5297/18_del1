effb63d2d5a8e7f41f7e18b9c6cfe8f45e3a92bb
Johannes <s185114@student.dtu.dk>
2018-10-08 16:12:55 +0200

    Ønske 2 implementeret

--- 18_del1/src/TerningSpil.java
+++ 18_del1/src/TerningSpil.java
@@ -12,50 +12,63 @@ public class TerningSpil {
         Scanner Spil = new Scanner(System.in);
         final int MAX = 40;
         int sum = 0, sum1 = 0, sum2 = 0, sum3 = 0;
+        int k1,k2,k3,k4;
 
         // Hvis spiller 1 taster 2, så vil programmet kaste terningen, og lægge de to slået værdier sammen
 
 
         while (MAX > sum1 && MAX > sum3) {
             System.out.println();
-                System.out.println("Spiller nr.1 kaster");
+            System.out.println("Spiller nr.1 kaster");
+            do {
                 System.out.println("Tast 2 for at kaste terningen: ");
-            if (Spil.hasNextInt() && Spil.nextInt() == 2) {
-                int k1 = tern.kast();
-                int k2 = tern.kast();
-                sum = k1 + k2;
-                sum1 = sum + sum1;
-                System.out.println(k1 + " " + k2);
-                if (k1 + k2 == 2){
-                    System.out.println("Spiller 1. mister alle sine point.");
-                    sum1=0;
-                } else {
-                    System.out.println("Summen af kastet er: " + sum);
-                }
-                System.out.println("Spiller nr.1 har " + sum1 + " point");
-            } else {
-                System.out.println("Forkert input... Tast 2 for at kaste terningen: ");
-            }
-            if (MAX > sum1) {  //"&& MAX > sum3" behøves ikke, eftersom dette er forsikret af vores løkke, og er endu ikke blevet ændret inden i løkken.
-                System.out.println();
-                System.out.println("Spiller nr.2 kaster");
-                System.out.println("Tast 2 for at kaste terningen: ");
+                 k1 = tern.kast();
+                 k2 = tern.kast();
                 if (Spil.hasNextInt() && Spil.nextInt() == 2) {
-                    int k3 = tern.kast();
-                    int k4 = tern.kast();
-                    sum2 = k3 + k4;
-                    sum3 = sum2 + sum3;
-                    System.out.println(k3 + " " + k4);
-                    if (k3 + k4 == 2){
+                    sum = k1 + k2;
+                    sum1 = sum + sum1;
+                    System.out.println(k1 + " " + k2);
+                    if (k1 + k2 == 2) {
                         System.out.println("Spiller 1. mister alle sine point.");
-                        sum3=0;
+                        sum1 = 0;
                     } else {
-                        System.out.println("Summen af kastet er: " + sum2);
+                        System.out.println("Summen af kastet er: " + sum);
                     }
-                    System.out.println("Spiller nr.2 har " + sum3 + " point");
+                    System.out.println("Spiller nr.1 har " + sum1 + " point");
                 } else {
                     System.out.println("Forkert input... Tast 2 for at kaste terningen: ");
                 }
+                if(k1==k2 && MAX > sum1 && k1+k2!=2){
+                    System.out.println();
+                    System.out.println("Spiller 1. får et ekstra slag");
+                }
+            }while(k1 == k2 && MAX > sum1 && k1+k2!=2);
+            if (MAX > sum1) {  //"&& MAX > sum3" behøves ikke, eftersom dette er forsikret af vores løkke, og er endu ikke blevet ændret inden i løkken.
+                System.out.println();
+                System.out.println("Spiller nr.2 kaster");
+                do {
+                    System.out.println("Tast 2 for at kaste terningen: ");
+                    k3 = tern.kast();
+                    k4 = tern.kast();
+                    if (Spil.hasNextInt() && Spil.nextInt() == 2) {
+                        sum2 = k3 + k4;
+                        sum3 = sum2 + sum3;
+                        System.out.println(k3 + " " + k4);
+                        if (k3 + k4 == 2) {
+                            System.out.println("Spiller 1. mister alle sine point.");
+                            sum3 = 0;
+                        } else {
+                            System.out.println("Summen af kastet er: " + sum2);
+                        }
+                        System.out.println("Spiller nr.2 har " + sum3 + " point");
+                    } else {
+                        System.out.println("Forkert input... Tast 2 for at kaste terningen: ");
+                    }
+                    if(k3==k4 && MAX > sum3 && k3+k4!=2){
+                        System.out.println();
+                        System.out.println("Spiller 2. får et ekstra slag");
+                    }
+                }while(k3==k4 && MAX > sum3 && k3+k4!=2);
             }
         }
         if (MAX <= sum1) {
