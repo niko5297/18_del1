48b9357f5508904a30d61a09a591573115315b23
nikolajwassmann <nwassmann@hotmail.com>
2018-10-08 16:43:52 +0200

    JUnit test på Terning og rettelse i Terning

--- 18_del1/src/Terning.java
+++ 18_del1/src/Terning.java
@@ -7,10 +7,5 @@ public class Terning {
         int t3 = Math.round(t2);
         return t3 + 1;
     }
-    public void kastGange(int g){
-        for(int k=0; k<g; k++){
-            System.out.print(kast() + " ");
-        }
-    }
 
 }
--- 18_del1/src/TerningTest.java
+++ 18_del1/src/TerningTest.java
@@ -1,12 +1,17 @@
 import org.junit.Test;
 
+import static org.junit.Assert.assertTrue;
+
 public class TerningTest {
+
+    Terning T = new Terning();
 
     @Test
     public void kast() {
-    }
+        for (int i = 0; i < 60000; i++) {
+            int tal = T.kast();
+            assertTrue(tal > 0 && tal < 7);
+        }
 
-    @Test
-    public void kastGange() {
     }
 }
